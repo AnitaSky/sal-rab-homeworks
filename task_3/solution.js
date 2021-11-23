9cd391cf859b20c5d539c65e6a743f92d42d86f4
AnitaSky <anita.shiryaeva@gmail.com>
2021-11-23 16:47:07 +0300

    quotes v3

--- task_3/solution.js
+++ task_3/solution.js
@@ -4,13 +4,13 @@ function renderCartItem(item) {
     let itemCountText;
     let itemCountText = "";
     let itemCountText = itemCountText + product.count; 
-    let itemCountText = itemCountText + ` × `;
+    let itemCountText = itemCountText + ' × ';
     let itemCountText = itemCountText + product.price;
-    let itemCountText = itemCountText + ` ₽ = `;
+    let itemCountText = itemCountText + ' ₽ = ';
     let sum;
     let sum = product.count * product.price;
     let itemCountText = itemCountText + sum;
-    let itemCountText = itemCountText + ` ₽`;
+    let itemCountText = itemCountText + ' ₽';
 
 return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
 }

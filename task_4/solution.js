ef9fd9ebb8ce389ad13eb1b43a3bffbadce91862
AnitaSky <anita.shiryaeva@gmail.com>
2021-11-24 22:40:00 +0300

    task 4.1 v 2.0

--- task_4/solution.js
+++ task_4/solution.js
@@ -4,19 +4,31 @@ function parseProducts(json) {
     // Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
-   let data = JSON.parse(json); // в объект с помощью функции JSON.parse(json)
+    // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
-    let products = data.products;
-    return data(products);// Верните как результат функции свойство products объекта data
+    // Верните как результат функции свойство products объекта data
+    let data;
+    data = JSON.parse(json);
+    let products;
+    products = data.products;
+    
+    return  products;
 }
 
 function renderProductsCards(json) {// Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
-clearProducts(); // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
-let products = parseProducts(json);// Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
-let length = products.length;// Запишите в переменную length значение свойства products.length
-for (let i = 0; i < length; i += 1){
-    addProduct(products[i]);
-}; // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
+// Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
+// Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
+// Запишите в переменную length значение свойства products.length
+// Напишите цикл, в котором перебираете все products от 0 до (length - 1)
 // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+
+clearProducts();
+let products;
+products = parseProducts(json);
+let length;
+length = products.length;
+for (let i = 0; i < length; i += 1) {
+    addProduct(products[i]);
+ }
 }

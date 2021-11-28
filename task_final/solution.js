718e43ce88b0a50d6e2218e1a8fd959e8dc2003c
AnitaSky <anita.shiryaeva@gmail.com>
2021-11-28 16:43:39 +0300

    v 2.4

--- task_final/solution.js
+++ task_final/solution.js
@@ -40,7 +40,7 @@ function sendRequest(name, phone, address, goods, sum) {
     let data.order.address = address;
     let data.order.sum = sum;
 
-    let goods = [title, count];
+    //let goods = [title, count];
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
